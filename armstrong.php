172ce56ed7b200e050f3d4737c9116f1b379707f
ALSTINGLORIA <144704585+ALSTINGLORIA@users.noreply.github.com>
2025-09-15 21:10:55 +0530

    Update armstrong.php

--- armstrong.php
+++ armstrong.php
@@ -1,16 +1,19 @@
 <?php
-$num = 153;
+echo "Enter a number: ";
+$num = trim(fgets(STDIN));
 $originalNum = $num;
 $sum = 0;
 $length = strlen((string)$num);
+
 while ($num > 0) {
     $digit = $num % 10;
     $sum += pow($digit, $length);
     $num = (int)($num / 10);
 }
+
 if ($sum == $originalNum) {
-    echo "$originalNum is an Armstrong number.";
+    echo "$originalNum is an Armstrong number.\n";
 } else {
-    echo "$originalNum is not an Armstrong number.";
+    echo "$originalNum is not an Armstrong number.\n";
 }
 ?>
